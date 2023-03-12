3066de16c28738ee2ea44ffed69807478d9fa0f6
Konrad Amtenbrink <konrad02@gmx.de>
2023-03-12 22:02:43 +0100

    feat: add device disconnect

--- src/main.m
+++ src/main.m
@@ -1,14 +1,41 @@
-#import <Foundation/Foundation.h>
 #import <IOBluetooth/IOBluetooth.h>
 
-void list_paired_devices() {
-  for (IOBluetoothDevice *device in [IOBluetoothDevice pairedDevices]) {
-    printf(", name: \"%s\"", [device name] ? [[device name] UTF8String] : "-");
+@interface DeviceNotificationRunLoopStopper : NSObject
+@end
+@implementation DeviceNotificationRunLoopStopper {
+  IOBluetoothDevice *expectedDevice;
+}
+- (id)initWithExpectedDevice:(IOBluetoothDevice *)device {
+  expectedDevice = device;
+  return self;
+}
+- (void)notification:(IOBluetoothUserNotification *)notification fromDevice:(IOBluetoothDevice *)device {
+  if ([expectedDevice isEqual:device]) {
+    [notification unregister];
+    CFRunLoopStop(CFRunLoopGetCurrent());
   }
 }
+@end
 
-int main(int argc, char** argv)
-{
-  list_paired_devices();
-  return 0;
+
+void list_devices(NSArray *devices ) {
+    for (IOBluetoothDevice *device in devices) {
+        NSLog(@"Device: %@", device);
+    }
+}
+
+void disconnect_device(IOBluetoothDevice *device) {
+    DeviceNotificationRunLoopStopper *stopper =
+        [[[DeviceNotificationRunLoopStopper alloc] initWithExpectedDevice:device] autorelease];
+
+    [device registerForDisconnectNotification:stopper selector:@selector(notification:fromDevice:)];
+
+    if ([device closeConnection] != kIOReturnSuccess) {
+        NSLog(@"Failed to close connection to device: %@", device);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    list_devices([IOBluetoothDevice pairedDevices]);
+    return 0;
 }
